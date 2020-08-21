593393ccf0c2da7a9060c002ee8ead7812019a93
simoneletizi <simone.letizi@studio.unibo.it>
2020-08-21 17:49:51 +0200

    tests competed and refactored close #8

--- src/test/scala/it/unibo/intelliserra/server/ExampleEntityManagerSpec.scala
+++ src/test/scala/it/unibo/intelliserra/server/ExampleEntityManagerSpec.scala
@@ -1,6 +1,6 @@
 package it.unibo.intelliserra.server
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import it.unibo.intelliserra.core.entity.{ActingCapability, SensingCapability}
 import it.unibo.intelliserra.core.sensor.Category
@@ -33,20 +33,29 @@ private class ExampleEntityManagerSpec extends TestKit(ActorSystem("MySpec"))
 
   "An Entity Manager" must {
     "register a sensor after receiving join" in {
-      val sensorProbe = TestProbe()
-      entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
-      expectMsg(JoinOK)
-      entityManager.underlyingActor.entities shouldBe Map(RegisteredSensor(mockSensorID, mockSensorCapability) -> sensorProbe.ref)
+      val sensorActorProbe = TestProbe()
+      sendJoinEntityMessage(JoinSensor(mockSensorID, mockSensorCapability, sensorActorProbe.ref))
+    }
+  }
+
+  "An Entity Manager" must {
+    "register an actuator after receiving join" in {
+      val actuatorActorProbe = TestProbe()
+      sendJoinEntityMessage(JoinActuator(mockActuatorID, mockActuatorCapability, actuatorActorProbe.ref))
     }
   }
 
   "An Entity Manager " should  {
-    "not permit adding of entity with existing identifier" in {
-      val sensorProbe = TestProbe()
-      entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
-      entityManager.underlyingActor.entities should have size 1
-      entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
-      entityManager.underlyingActor.entities should have size 1
+    "not permit adding of sensor with existing identifier" in {
+      val sensorActorProbe = TestProbe()
+      checkNoDuplicateInsertion(JoinSensor(mockSensorID, mockSensorCapability, sensorActorProbe.ref))
+    }
+  }
+
+  "An Entity Manager " should  {
+    "not permit adding of actuator with existing identifier" in {
+      val actuatorActorProbe = TestProbe()
+      checkNoDuplicateInsertion(JoinSensor(mockSensorID, mockSensorCapability, actuatorActorProbe.ref))
     }
   }
 
@@ -54,6 +63,26 @@ private class ExampleEntityManagerSpec extends TestKit(ActorSystem("MySpec"))
     "have no entities" in {
       entityManager.underlyingActor.entities shouldBe Map()
     }
+  }
+
+  private def sendJoinEntityMessage(joinRequestMessage: JoinRequest){
+    entityManager ! joinRequestMessage
+    expectMsg(JoinOK)
+    joinRequestMessage match {
+      case JoinSensor(identifier, sensingCapability, sensorRef) => {
+        entityManager.underlyingActor.entities shouldBe Map(RegisteredSensor(identifier, sensingCapability) -> sensorRef)
+      }
+      case JoinActuator(identifier, actingCapability, actuatorRef) => {
+        entityManager.underlyingActor.entities shouldBe Map(RegisteredActuator(identifier, actingCapability) -> actuatorRef)
+      }
+    }
+  }
+
+  private def checkNoDuplicateInsertion(joinRequest: JoinRequest) = {
+    entityManager ! joinRequest
+    entityManager.underlyingActor.entities should have size 1
+    entityManager ! joinRequest
+    entityManager.underlyingActor.entities should have size 1
   }
 
   override def afterAll: Unit = {
