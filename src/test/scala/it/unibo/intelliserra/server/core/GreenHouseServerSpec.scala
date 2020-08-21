8913fe17059e68a3b1f7f597b7ebdb9e83240a13
Andrea <petretiandrea@gmail.com>
2020-08-21 10:45:28 +0200

    first impl Measure

--- src/test/scala/it/unibo/intelliserra/server/core/GreenHouseServerSpec.scala
+++ src/test/scala/it/unibo/intelliserra/server/core/GreenHouseServerSpec.scala
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfter, Matchers, WordSpecLike}
 import org.scalatestplus.junit.JUnitRunner
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 @RunWith(classOf[JUnitRunner])
 class GreenHouseServerSpec extends WordSpecLike
@@ -28,8 +29,10 @@ class GreenHouseServerSpec extends WordSpecLike
   "Green house server facade " should {
 
     "allow to start server with success" in {
-      Await.result(server.start(), WAIT_FUTURE)
-      succeed
+      Try(Await.ready(server.start(), WAIT_FUTURE)) match {
+        case Failure(exception) => fail(exception)
+        case Success(_) => succeed
+      }
     }
 
     "raise IllegalStateException when start is called on already running instance" in {
