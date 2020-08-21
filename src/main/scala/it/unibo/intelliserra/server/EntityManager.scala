47a416824e225b0b18b92cf5ec5b4492364ca0c2
simoneletizi <simone.letizi@studio.unibo.it>
2020-08-21 12:23:16 +0200

    registered entities created for refactoring; added test for identifier duplication

--- src/main/scala/it/unibo/intelliserra/server/EntityManager.scala
+++ src/main/scala/it/unibo/intelliserra/server/EntityManager.scala
@@ -1,40 +1,53 @@
 package it.unibo.intelliserra.server
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import it.unibo.intelliserra.core.entity.{ActingCapability, SensingCapability}
-import it.unibo.intelliserra.server.EntityManager.{JoinActuator, JoinError, JoinOK, JoinSensor, RegisteredActuator, RegisteredSensor}
-
-import scala.util.Try
+import it.unibo.intelliserra.core.entity.{ActingCapability, Capability, SensingCapability}
+import it.unibo.intelliserra.server.EntityManager.{JoinActuator, JoinError, JoinOK, JoinResult, JoinSensor, RegisteredActuator, RegisteredEntity, RegisteredSensor}
 
 class EntityManagerActor extends Actor{
 
-  private[server] var sensors : Map[RegisteredSensor, ActorRef] = Map()
-  private[server] var actuators : Map[RegisteredActuator, ActorRef] = Map()
+  private[server] var entities : Map[RegisteredEntity, ActorRef] = Map()
 
   override def receive: Receive = {
     case JoinSensor(identifier, capabilities, sensorRef) => {
-      val joinResult = Some(JoinOK).filterNot(_ => checkSensorExistence(identifier))
-                                    .foreach(_ => sensors = sensors + (RegisteredSensor(identifier,capabilities) -> sensorRef))
-
-      sender ! joinResult
+      addEntityIfNotExists(RegisteredSensor(identifier, capabilities),sensorRef) match {
+        case Some(newMap) => entities = newMap ; sender ! JoinOK
+        case None => sender ! JoinError
+      }
+    }
+    case JoinActuator(identifier, capabilities, sensorRef) => {
+      addEntityIfNotExists(RegisteredActuator(identifier, capabilities),sensorRef).fold()
+      addEntityIfNotExists(RegisteredActuator(identifier, capabilities),sensorRef) match {
+        case Some(newMap) => entities = newMap ; sender ! JoinOK
+        case None => sender ! JoinError
+      }
     }
   }
 
-  private def checkSensorExistence(identifier:String) = sensors.exists(_._1.identifier == identifier)
-  //private def checkActuatorExistence(identifier: String) = actuators.exists(_._1.identifier == identifier)
+  private def addEntityIfNotExists(registeredEntity: RegisteredEntity, actorRef: ActorRef) : Option[Map[RegisteredEntity, ActorRef]] = {
+    entities.find(pair => pair._1.identifier == registeredEntity.identifier)
+            .fold[Option[Map[RegisteredEntity, ActorRef]]](Some(entities + (registeredEntity -> actorRef)))(_ => None)
+  }
 }
 
 object EntityManager{
   val name = "entityManager"
-  def apply(implicit actorSystem: ActorSystem): ActorRef = {
+
+  def apply()(implicit actorSystem: ActorSystem): ActorRef = {
     actorSystem.actorOf(Props[EntityManagerActor], name)
   }
-  case class RegisteredActuator(identifier: String, capability : ActingCapability)
-  case class RegisteredSensor(identifier: String, capability: SensingCapability)
+
+  sealed trait RegisteredEntity{
+    val identifier : String
+    val capabilities : Capability
+  }
+  case class RegisteredActuator(override val identifier: String, override val capabilities : ActingCapability) extends RegisteredEntity
+  case class RegisteredSensor(override val identifier: String, override val capabilities: SensingCapability) extends RegisteredEntity
+
   case class JoinSensor(identifier: String, sensingCapability: SensingCapability, sensorRef: ActorRef)
   case class JoinActuator(identifier: String, actingCapability: ActingCapability, actuatorRef : ActorRef)
 
-  trait JoinResult
+  sealed trait JoinResult
   case object JoinOK extends JoinResult
-  case object JoinError extends JoinResult
+  case class JoinError(error: String) extends JoinResult
 }
--- src/test/scala/it/unibo/intelliserra/server/ExampleEntityManagerSpec.scala
+++ src/test/scala/it/unibo/intelliserra/server/ExampleEntityManagerSpec.scala
@@ -4,20 +4,20 @@ import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import it.unibo.intelliserra.core.entity.{ActingCapability, SensingCapability}
 import it.unibo.intelliserra.core.sensor.Category
-import it.unibo.intelliserra.server.EntityManager.{JoinSensor, RegisteredActuator, RegisteredSensor}
+import it.unibo.intelliserra.server.EntityManager.{JoinOK, JoinSensor, RegisteredActuator, RegisteredSensor}
 import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpecLike}
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-private class ExampleEntityManagerTest extends TestKit(ActorSystem("MySpec"))
+private class ExampleEntityManagerSpec extends TestKit(ActorSystem("MySpec"))
   with ImplicitSender
   with Matchers
   with WordSpecLike
   with BeforeAndAfter
   with BeforeAndAfterAll  {
 
-  private var entityManager : TestActorRef[EntityManagerActor] = TestActorRef.create[EntityManagerActor](system, Props[EntityManagerActor])
+  private var entityManager : TestActorRef[EntityManagerActor] = _
   private val mockSensorID = "sensorID"
   private val mockSensorCapability = SensingCapability(Temperature)
   private val mockActuatorID = "actuatorID"
@@ -26,12 +26,32 @@ private class ExampleEntityManagerTest extends TestKit(ActorSystem("MySpec"))
   case object Temperature extends Category
   case object Humidity extends Category
 
+  before{
+    entityManager = TestActorRef.create[EntityManagerActor](system, Props[EntityManagerActor])
+  }
+
   "An Entity Manager" must {
     "register a sensor after receiving join" in {
       val sensorProbe = TestProbe()
-
       entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
-      entityManager.underlyingActor.sensors shouldBe Map(RegisteredSensor(mockSensorID, mockSensorCapability) -> sensorProbe.ref)
+      expectMsg(JoinOK)
+      entityManager.underlyingActor.entities shouldBe Map(RegisteredSensor(mockSensorID, mockSensorCapability) -> sensorProbe.ref)
+    }
+  }
+
+  "An Entity Manager " should  {
+    "not permit adding of entity with existing identifier" in {
+      val sensorProbe = TestProbe()
+      entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
+      entityManager.underlyingActor.entities should have size 1
+      entityManager ! JoinSensor(mockSensorID, mockSensorCapability, sensorProbe.ref)
+      entityManager.underlyingActor.entities should have size 1
+    }
+  }
+
+  "An Entity Manager just created " should  {
+    "have no entities" in {
+      entityManager.underlyingActor.entities shouldBe Map()
     }
   }
 
