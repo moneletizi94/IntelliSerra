6c5b593349f4434458539ff5460a9a57e18cb97f
Andrea Petreti <petretiandrea@gmail.com>
2020-08-28 16:02:30 +0200

    fixing stash

--- src/main/scala/it/unibo/intelliserra/core/sensor/Sensor.scala
+++ src/main/scala/it/unibo/intelliserra/core/sensor/Sensor.scala
@@ -1,9 +1,41 @@
 package it.unibo.intelliserra.core.sensor
 
+import java.util.concurrent.Executors
+
 import it.unibo.intelliserra.core.entity.SensingCapability
+import it.unibo.intelliserra.core.sensor.MeasureProducer.PushProducer
+import it.unibo.intelliserra.core.sensor.Sensor.BasicSensor
+import monix.execution.Scheduler
+import monix.reactive.Observable
+import monix.reactive.subjects.PublishSubject
+
+import scala.concurrent.duration.FiniteDuration
+
+
 //TODO check for refactory with Actuator
 trait Sensor {
   def identifier: String
   def capability: SensingCapability
-  def state: Measure
+  def measures: Observable[Measure]
+}
+
+object Sensor {
+
+  def apply(identifier: String, capability: SensingCapability)(behaviour: MeasureProducer): Sensor =
+    new SensorWithBehaviour(identifier, capability, behaviour)
+
+  private[sensor] class SensorWithBehaviour(override val identifier: String,
+                                            override val capability: SensingCapability,
+                                            override val behaviour: MeasureProducer) extends BasicSensor
+
+  abstract class BasicSensor extends Sensor {
+    override def measures: Observable[Measure] = behaviour.measures//.filter(m => m.category == capability.category)
+    protected def behaviour: MeasureProducer
+  }
+
+}
+
+object Example extends App {
+
+  case object Temperature extends Category
 }
--- src/main/scala/it/unibo/intelliserra/device/DeviceDeploy.scala
+++ src/main/scala/it/unibo/intelliserra/device/DeviceDeploy.scala
@@ -3,17 +3,16 @@ package it.unibo.intelliserra.device
 import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.ask
 import akka.util.Timeout
-import it.unibo.intelliserra.client.core.GreenHouseClient
 import it.unibo.intelliserra.common.akka.RemotePath
 import it.unibo.intelliserra.common.akka.configuration.GreenHouseConfig
 import it.unibo.intelliserra.common.communication.Messages.{JoinActuator, JoinError, JoinOK, JoinSensor}
 import it.unibo.intelliserra.core.actuator.Actuator
 import it.unibo.intelliserra.core.sensor.Sensor
-import it.unibo.intelliserra.server.{ActuatorActor, EntityManagerActor, SensorActor}
+import it.unibo.intelliserra.device.core.actuator.ActuatorActor
+import it.unibo.intelliserra.device.core.sensor.SensorActor
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import it.unibo.intelliserra.common.communication.Protocol._
 
 /**
  *
