9443ea1dbb63be2f53badad628b3ae5c4aad661a
simoneletizi <simone.letizi@studio.unibo.it>
2020-09-15 10:25:53 +0200

    generator working

--- src/test/scala/it/unibo/intelliserra/utils/Generator.scala
+++ src/test/scala/it/unibo/intelliserra/utils/Generator.scala
@@ -3,7 +3,7 @@ package it.unibo.intelliserra.utils
 import java.util.UUID
 
 import it.unibo.intelliserra.core.actuator.Action
-import it.unibo.intelliserra.core.sensor.{Category, IntType, Measure, NumericType, StringType, ValueType}
+import it.unibo.intelliserra.core.sensor.{Category, DoubleType, IntType, Measure, NumericType, StringType, ValueType}
 import it.unibo.intelliserra.utils.TestUtility.Actions.{Fan, Light, Water}
 import it.unibo.intelliserra.utils.TestUtility.Categories
 import it.unibo.intelliserra.utils.TestUtility.Categories.{Humidity, Temperature, Weather}
@@ -13,59 +13,64 @@ import scala.util.Random
 trait Sample[T]{
   def sample : T
 }
-trait SampleMeasureType[C <: Category[T],T <: ValueType] extends Sample[T]
+
+//abstract class MeasureValueSample[A <: ValueType](category: Category[A]) extends Sample[A]
 
 object Sample{
 
-  implicit case object WeatherValuesSample extends SampleMeasureType[Weather.type , StringType]{
+  /*implicit object WeatherValuesSample extends MeasureValueSample(Weather){
     override def sample: StringType = Generator.oneOf(Samples.WeatherCondition.toList)
   }
 
-  implicit case object IDSample extends Sample[StringType]{
+  implicit object HumidityValuesSample extends MeasureValueSample(Humidity){
+    override def sample: IntType = (Random.nextDouble()*100).toInt
+  }
+
+  implicit object TemperatureMeasureSample extends MeasureValueSample(Temperature){
+    override def sample: IntType = 20 + Random.nextInt( (30 - 20) + 1 )
+  }*/
+
+  implicit object WeatherValuesSample extends Sample[StringType]{
+    override def sample: StringType = Generator.oneOf(Samples.WeatherCondition.toList)
+  }
+
+  implicit object HumidityValuesSample extends Sample[IntType]{
+    override def sample: IntType = (Random.nextDouble()*100).toInt
+  }
+
+
+  implicit object TemperatureMeasureSample extends Sample[IntType]{
+    override def sample: IntType = 20 + Random.nextInt( (30 - 20) + 1 )
+  }
+
+  implicit object IDSample extends Sample[StringType]{
     override def sample: StringType = UUID.randomUUID().toString
   }
 
-  trait IntRangeSample extends Sample[IntType]{
-    //from scala 2.13 is available Random.between
-    def range: Range
-    override def sample: IntType = Random.nextInt((range.end - range.start) + 1)
-  }
-
-  implicit case class TemperatureMeasureSample(override val range: Range = 10 to 30) extends SampleMeasureType[Temperature.type , IntType] with IntRangeSample
 
   implicit case object IntRandomSample extends Sample[IntType]{
     override def sample: IntType = Random.nextInt()
   }
 
-  implicit case object CategorySample extends Sample[Category[_]]{
+  implicit object CategorySample extends Sample[Category[_]]{
     override def sample: Category[_] = Generator.oneOf(Samples.CategoriesSample)
   }
 
-  implicit case object ActionSample extends Sample[Action]{
+  implicit object ActionSample extends Sample[Action]{
     override def sample: Action = Generator.oneOf(Samples.Actions)
   }
 
-  implicit case object MeasuresSample extends Sample[Measure]{
-    import Sample._
-
-    override def sample: Measure = ???/*implicitly[Sample[Category[_]]].sample match {
-      case Temperature => MeasureSample(Temperature)
-      case Humidity => MeasureSample(Humidity).sample
-      case Weather => MeasureSample(Weather)(Sample.WeatherValuesSample).sample
-    }*/
+  implicit object MeasuresSample extends Sample[Measure]{
+    override def sample: Measure = implicitly[Sample[Category[_]]].sample match {
+      case Temperature => MeasureSample(Temperature)(TemperatureMeasureSample).sample
+      case Humidity => MeasureSample(Humidity)(HumidityValuesSample).sample
+      case Weather => MeasureSample(Weather)(WeatherValuesSample).sample
+    }
   }
 
-  /*implicit case object TemperatureSample extends Sample[Temperature.type ] {
-    override def sample: Categories.Temperature.type = ???
-  }*/
-/*
-  case class MeasureSample[A <: ValueType, B : Category[_]](category: B[A])(implicit typeSample : SampleMeasureType[ , A]) extends Sample[Measure]{
-    override def sample: Measure = ???Measure(category)(typeSample.sample)
-  }*/
-/*
-  case class MeasureSample[A <: ValueType](category: Category[A])(implicit typeSample : Sample[A]) extends Sample[Measure]{
-    override def sample: Measure = Measure(category)(typeSample.sample)
-  }*/
+  case class MeasureSample[A <: ValueType](cat: Category[A])(implicit valueTypeSample : Sample[A]) extends Sample[Measure]{
+    override def sample: Measure = Measure(cat)(valueTypeSample.sample)
+  }
 
 }
 
@@ -88,8 +93,10 @@ object Samples {
 object Prova extends App{
   import Sample._
   import Generator._
-  generate[IntType]
-  generateMore[StringType](10)
-  println(Generator.generateMore[Measure](10))
-  //println(Generator.generateMore[Measure](10)(MeasureSample(Temperature)))
+
+  println(generate[Measure])
+  println(generateMore[StringType](10)(IDSample))
+  println(generateMore[Measure](10))
+  println(generateMore[Measure](10)(MeasureSample(Humidity)(HumidityValuesSample)))
+
 }
