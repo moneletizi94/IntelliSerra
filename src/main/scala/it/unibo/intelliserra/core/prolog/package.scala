54bbb006a4183a4a11e5604888c52977e77ec1da
Ylenia Battistini <ylenia.battistini@studio.unibo.it>
2020-09-20 12:31:45 +0200

    refactoring prolog engine with RichProlog in package object and fix documentation

--- src/main/scala/it/unibo/intelliserra/core/prolog/package.scala
+++ src/main/scala/it/unibo/intelliserra/core/prolog/package.scala
@@ -1,12 +1,29 @@
 package it.unibo.intelliserra.core
 
-import alice.tuprolog.Term
+import alice.tuprolog.{Prolog, Struct, Term, Theory}
+import it.unibo.intelliserra.core.rule.Rule
+
+import scala.collection.JavaConverters._
 
 package object prolog {
-  import it.unibo.intelliserra.core.prolog.Representations._
   implicit class RichAny[A : PrologRepresentation](toEnrich : A) {
     def toTerm : Term = {
       implicitly[PrologRepresentation[A]].toTerm(toEnrich)
+    }
+  }
+
+  implicit class RichProlog(engine: Prolog){
+
+     def assert(ruleClause: Term) : Boolean = {
+       Theory.fromPrologList(ruleClause.castTo(classOf[Struct])).getClauses.asScala
+         .foreach(rule => engine.solve(s"assert($rule)"))
+       true
+     }
+
+    def retract(ruleClause: Term) : Boolean = {
+      Theory.fromPrologList(ruleClause.castTo(classOf[Struct])).getClauses.asScala
+        .foreach(rule => engine.solve(s"retract($rule)"))
+      true
     }
   }
 }
