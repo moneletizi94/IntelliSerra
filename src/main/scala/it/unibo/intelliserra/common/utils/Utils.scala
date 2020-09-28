0f21cb05f20ae023bf26de1a84196ae63d107afd
simoneletizi <simone.letizi@studio.unibo.it>
2020-09-28 16:10:44 +0200

    documentation of RegisteredDevice, RepeatedAction, State, ValueType done

--- src/main/scala/it/unibo/intelliserra/common/utils/Utils.scala
+++ src/main/scala/it/unibo/intelliserra/common/utils/Utils.scala
@@ -1,11 +1,5 @@
 package it.unibo.intelliserra.common.utils
 
-import java.util
-
-import akka.actor.AbstractActor.ActorContext
-import akka.actor.{Actor, ActorLogging}
-import it.unibo.intelliserra.server.aggregation.Aggregator
-
 import scala.util.Try
 
 object Utils {
@@ -14,13 +8,6 @@ object Utils {
     val (successes, failures) = iterable.partition(_.isSuccess)
     failures.map(_ => ifFailure)
     successes.flatMap(_.toOption).map(ifSuccess(_))
-  }
-
-  // TODO: togliere
-  trait MessageReceivingLog { this : Actor with ActorLogging =>
-      def logReceiving(message : Any) : Unit = {
-        log.info(s"(${context.self.path.name}): Received message $message from ${context.sender.path.name}")
-      }
   }
 
 }
