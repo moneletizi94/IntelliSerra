1453f45dffacd992db21407d38bccb24d0d2cb9f
Andrea <petretiandrea@gmail.com>
2020-09-28 16:00:25 +0200

    update common package documentation

--- src/main/scala/it/unibo/intelliserra/common/akka/RemotePath.scala
+++ src/main/scala/it/unibo/intelliserra/common/akka/RemotePath.scala
@@ -4,14 +4,27 @@ import akka.actor.ActorPath
 
 object RemotePath {
 
-  // TODO: documentation
-  def entityManager(greenHouseName: String, serverHost: String, serverPort: Int): String =
-    buildRemotePath(greenHouseName, serverHost, serverPort, "/user/entityManager").toString
-
-  // TODO: documentation
+  /**
+   * Create a URI to remote server actor endpoint
+   * @param greenHouseName  the name of greenhouse
+   * @param serverHost      the hostname where the server is running
+   * @param serverPort      the port where the server is running
+   * @return
+   */
   def server(greenHouseName: String, serverHost: String, serverPort: Int): String =
     buildRemotePath(greenHouseName, serverHost, serverPort, "/user/serverActor").toString
 
+
+  /**
+   * Create a URI of remote EntityManager actor endpoint
+   * @param greenHouseName  the name of greenhouse
+   * @param serverHost      the hostname where the server is running
+   * @param serverPort      the port where the server is running
+   * @return
+   */
+  def entityManager(greenHouseName: String, serverHost: String, serverPort: Int): String =
+    buildRemotePath(greenHouseName, serverHost, serverPort, "/user/entityManager").toString
+  
   private def buildRemotePath(greenHouseName: String,
                               serverHost: String,
                               serverPort: Int,
